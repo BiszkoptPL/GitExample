7d040699f1fdc02eb4726d48d408338df349edcb
Maciej <maciej502@wp.pl>
2017-01-11 22:42:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/maciej/gitexamples/GITexampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.maciej.gitexamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GITexampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gitexample);
+    }
+}
